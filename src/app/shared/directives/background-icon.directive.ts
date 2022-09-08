2d9c5e5e0d21589e43dab5bcf9609a73639c00e7
Siarhei Chura <siarhei.chura@connectwise.com>
2022-09-08 22:09:46 +0400

    add background icons

---
+++ src/app/shared/directives/background-icon.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core'
+
+import { IconService } from '@services/icon.service'
+import { IconConfigI } from '@shared/interfaces/Icon.interface'
+
+@Directive({
+  selector: '[appBackgroundIcon]'
+})
+export class BackgroundIconDirective implements OnInit {
+  @Input() config: IconConfigI
+
+  constructor(private el: ElementRef, private iconService: IconService) {}
+
+  ngOnInit() {
+    this.setElement()
+  }
+
+  setElement() {
+    this.iconService.config$.subscribe((resp) => {
+      this.el.nativeElement.style.fontSize = '22px'
+      this.el.nativeElement.style.position = 'absolute'
+      this.el.nativeElement.style.top = this.config.top
+      this.el.nativeElement.style.left = this.config.left
+      this.el.nativeElement.style.transform = `translateX(${resp.x * this.config.xx}px) translateY(${resp.y * this.config.xy}px) rotate(${this.config.rotate})`
+    })
+  }
+}
---
+++ src/app/shared/interfaces/Icon.interface.ts
@@ -0,0 +1,18 @@
+export interface IconTranslateI {
+  x: number
+  y: number
+}
+
+export interface IconConfigI {
+  icon: string
+  top: string
+  left: string
+  rotate: string
+  xx: number
+  xy: number
+}
+
+export interface IconSvgI {
+  name: string
+  svg: string
+}
