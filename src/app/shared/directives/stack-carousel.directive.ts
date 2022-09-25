3f2fda5c9a27f64dad4f2d3eb6debe760fcce62f
Siarhei Chura <chura.siarhei@gmail.com>
2022-09-25 12:41:45 +0400

    S-6: add skill stack carousel directive

---
+++ src/app/shared/directives/stack-carousel.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, ElementRef, OnInit} from "@angular/core";
+
+@Directive({
+  selector: '[appStackCarousel]'
+})
+export class StackCarouselDirective implements OnInit {
+  y: number = 0
+
+  constructor(private el: ElementRef) {
+    this.el.nativeElement.style.transition = '0.5s'
+  }
+
+  ngOnInit() {
+    this.setElement()
+  }
+
+  setElement() {
+    setInterval(() => {
+      this.y = this.y === -140 ? 0 : this.y - 35
+      this.el.nativeElement.style.transform = `translateY(${this.y}px)`
+    }, 1200)
+  }
+}
